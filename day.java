33c03d8f250b6fbd08a75883fae494879e24bc46
ujwalsingla <ujwalsingla2577@gmail.com>
2019-11-20 13:49:15 +0530

    first commit

---
+++ day.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class day {
+
+    public static void main(String[] args)
+    {
+		String s="0";
+		System.out.println((int)(s.charAt(0))+" "+(int)(s.charAt(1)));    
+	
+}
+
+
+
+
+
+
+
